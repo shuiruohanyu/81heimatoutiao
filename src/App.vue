d7d28ae74cf50c54551f5b6df09993c7cf636618
gaoly <shuiruohanyu@foxmail.com>
2019-08-19 14:58:23 +0800

    登录模块-页面布局及样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
- <router-view />
+ <router-view  />
 </template>
 
-<style lang="less">
-
+<style lang='less' scoped>
+/* 认为你是css */
+// 如果加了lang 就会支持对应的css预处理器
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>登录页面</div>
+  <div class='login'>
+    <el-card class='login-card'>
+      <div class='title'>
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <!-- 表单结构  el-form el-form-item -->
+      <el-form>
+        <el-form-item>
+          <el-input placeHolder='请输入手机号'></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style='width:65%' placeHolder='请输入验证码'></el-input>
+          <el-button style='float:right'>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style='width:100%'>登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,6 +30,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+  .login {
+    width:100%;
+    height: 100vh;
+    background-image: url('../../assets/img/login_bg.jpg');
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+      width:420px;
+      height:360px;
+      .title {
+        text-align: center;
+        margin-bottom:30px;
+        img {
+          width:200px;
+          height:45px;
+        }
+      }
+    }
+  }
 </style>
