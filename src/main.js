118581bff0e0157da54c5c52f6fb810d943996c6
gaoly <shuiruohanyu@foxmail.com>
2019-08-19 16:58:32 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入element组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less' // 引文重置的基础样式
+import axios from 'axios' // 引入axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将后台接口的地址给axios的baseURL
+Vue.prototype.$axios = axios // 把axios插件给了Vue对象的原型属性
 Vue.use(ElementUI) // 注册element组件
 Vue.config.productionTip = false
 
