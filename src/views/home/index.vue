5e77ac009a9a5095b56c0ae9fe90620b9a338d8d
gaoly <shuiruohanyu@foxmail.com>
2019-08-20 10:12:43 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Home</div>
+<!-- 最外层容器 -->
+  <el-container>
+    <!-- 侧边栏容器 -->
+    <el-aside>
+      左侧内容
+    </el-aside>
+    <!-- 嵌套大容器 -->
+   <el-container>
+     <!-- 头部内容 -->
+     <el-header>头部内容</el-header>
+     <!-- 中间区域 -->
+     <el-main>中间区域</el-main>
+   </el-container>
+  </el-container>
 </template>
 
 <script>
