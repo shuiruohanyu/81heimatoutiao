46204133faaaddc791df4d1bc85abc293b503cb7
gaoly <shuiruohanyu@foxmail.com>
2019-08-20 10:51:37 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='layout-aside'>
+      <div class='title'>
+          <img src="../../assets/img/logo_admin.png" alt="">
+      </div>
+      <el-menu
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b">
+      <el-menu-item>
+          <i class='el-icon-s-home'></i>
+          <span>首页</span>
+          </el-menu-item>
+      <!-- 折叠菜单 -->
+      <el-submenu>
+        <!-- 具名插槽 传入内容 -->
+        <template slot='title'>
+         <i class='el-icon-menu' ></i>
+         <span>内容管理</span>
+        </template>
+         <!-- 匿名插槽 -->
+          <el-menu-item>发表文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+      </el-submenu>
+      <el-submenu>
+        <template slot='title'>
+         <i class='el-icon-user-solid' ></i>
+         <span>粉丝管理</span>
+        </template>
+         <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item>
+          <i class='el-icon-s-tools'></i>
+          <span>账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.layout-aside {
+    .title {
+        padding: 15px 0;
+        text-align: center;
+        background-color: #2e2f32;
+        img {
+            width:120px;
+            height:30px;
+        }
+    }
+}
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,8 +2,9 @@
 <!-- 最外层容器 -->
   <el-container>
     <!-- 侧边栏容器 -->
-    <el-aside>
-      左侧内容
+    <el-aside style='width:200px;height:100vh;background-color:#323745'>
+      <!-- 左侧菜单 -->
+     <layout-aside></layout-aside>
     </el-aside>
     <!-- 嵌套大容器 -->
    <el-container>
@@ -16,8 +17,11 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
