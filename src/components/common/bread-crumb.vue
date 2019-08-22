5e7fc38b8f1343d0e95a799f7c1540da67e7c839
gaoly <shuiruohanyu@foxmail.com>
2019-08-22 09:46:16 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-breadcrumb separator=">">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+        <!-- 定义具名插槽 -->
+        <slot name='title'></slot>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
