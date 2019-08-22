ae5a6e53a9031d8ec40b8b11ef4ddd4d72effaff
gaoly <shuiruohanyu@foxmail.com>
2019-08-22 16:23:25 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>素材管理</div>
+  <el-card>
+      <bread-crumb slot='header'>
+        <!-- 具名插槽 -->
+        <template slot='title'>素材管理</template>
+      </bread-crumb>
+      <!--el-tabs中 v-model属性绑定是当前子标签页的name -->
+       <el-tabs v-model="activeName" >
+        <el-tab-pane label="全部图片" name="first">
+
+        </el-tab-pane>
+        <el-tab-pane label="收藏图片" name="second">
+
+        </el-tab-pane>
+  </el-tabs>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'first'
+    }
+  }
 }
 </script>
 
