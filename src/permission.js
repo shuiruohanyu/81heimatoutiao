132ffba46aee8fb0ce3c2971924153746da7984b
gaoly <shuiruohanyu@foxmail.com>
2019-08-25 16:48:41 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,9 @@
 import router from './router'
+import nprogress from 'nprogress' // 引入文件
+import 'nprogress/nprogress.css' // 引入样式
 // 全局导航前置守卫
 router.beforeEach(function (to, from, next) {
+  nprogress.start() // 开始进度条
   // 如果是以home为起始 就认为 进入了需要检查token的区域
   if (to.path.startsWith('/home')) {
     // 获取前端的token
@@ -20,5 +23,8 @@ router.beforeEach(function (to, from, next) {
     next() // 直接放行
   }
   // 如果没有进去home的领地 就不需要再管了
+})
+router.afterEach(() => {
+  nprogress.done()
 })
 export default router
